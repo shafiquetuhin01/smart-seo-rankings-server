03d8d71b6a9e918c16d55dd48481c99049db2da0
unknown <sfqtuhin04@gmail.com>
2022-09-04 19:48:36 +0600

    database updating

--- index.js
+++ index.js
@@ -1,10 +1,88 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.visz4de.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+  try{
+    await client.connect();
+    const usersCollection = client.db("seoRankings").collection("user");
+    // update a doc 
+    app.put('/user/:email',async(req, res)=>{
+      const email = req .params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = {upsert: true};
+      const updatedDoc = {
+        $set: user
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+      res.send({result, token});
+
+    })
+    // post a item
+    app.post('/user', async(req, res) =>{
+      const newUser = req.body;
+      console.log("Adding new user", newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // get user
+    app.get('/user',async(req,res)=>{
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users); 
+    })
+
+    // delete a item 
+    app.delete('/user/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // find a individual user 
+    app.get('/user/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
+
+    // update a user 
+    app.put('/user/:id', async(req, res)=>{
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = {_id:ObjectId(id)};
+      const options = {upsert: true};
+      const updatedDoc = {
+        $set: {
+          name:updatedUser.name,
+          email:updatedUser.email
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
 
 const users = [
   { id: 1, name: "Shafique", age: 37, occupation: "service" },
@@ -30,6 +108,9 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+
+
 app.get("/user/:id", (req, res) => {
   console.log(req.params);
   const id = parseInt(req.params.id);
